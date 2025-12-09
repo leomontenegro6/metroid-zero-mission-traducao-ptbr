5f6a6095da614137285c6b34c96c403cf6cea677
Leomontenegro <leomontenegro6@gmail.com>
2025-12-08 22:49:13 -0300

    Corrigido bug menor nos scripts traduzidos do jogo vanilla.

--- Asm/textos_vanilla.asm
+++ Asm/textos_vanilla.asm
@@ -2,11 +2,11 @@
 .open "mzm.gba", 0x08000000
 .loadtable "./Tabelas/mzm.tbl"
 
-.include "./Asm/Scripts/" + version + "/script_1_pointers.asm"
-.include "./Asm/Scripts/" + version + "/script_2_pointers.asm"
+.include "./Asm/Scripts/vanilla/script_1_pointers.asm"
+.include "./Asm/Scripts/vanilla/script_2_pointers.asm"
 
 .org 0x08FDE2CC
-.include "./Asm/Scripts/" + version + "/script_1.asm"
-.include "./Asm/Scripts/" + version + "/script_2.asm"
+.include "./Asm/Scripts/vanilla/script_1.asm"
+.include "./Asm/Scripts/vanilla/script_2.asm"
 
 .close
